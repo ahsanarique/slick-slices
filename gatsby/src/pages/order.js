d5891e3f72e3a4ae8833d418014a8f9d89ad4310
ahsanarique <ahsan.arique@gmail.com>
2022-10-08 13:21:00 +0600

    initial commit

---
+++ gatsby/src/pages/order.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const OrderPage = () => (
+  <div>
+    <p>Order!</p>
+  </div>
+);
+
+export default OrderPage;
---
+++ gatsby/src/pages/slicemasters.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Nav from '../components/Nav';
+
+const SliceMastersPage = () => (
+  <div>
+    <Nav />
+    <p>Slice Masters!</p>
+  </div>
+);
+
+export default SliceMastersPage;
